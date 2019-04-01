0a3649d8ba806f9176e7b393cf6b38270268bf3e
Logan Ferise <loganferise@149-161-195-90.dhcp-bl.indiana.edu>
2019-04-01 15:22:42 -0400

    c

--- wc.py
+++ wc.py
@@ -1,14 +1,22 @@
 import sys
-#token counter
-counter = 0
+#Sylable counter
+counterspace = 0
 countersyl = 0
-
+counterchar = 0
+counterline = 0
 for c in sys.stdin.read():
+    counterchar = counterchar +1
     if c == ' ' :
-        counter = counter + 1
+        counterspace = counterspace + 1
     if c in 'aeiou' :
         countersyl = countersyl + 1
-print(counter)
+    if c == "\n" :
+        counterline = counterline + 1
+print(counterspace)
 print(countersyl)
-average =  countersyl / counter
+average =  countersyl / counterspace
 print (average)
+
+print (counterchar)
+print (counterline)
+#This method might nnot be totally accurate because vowels such as "y" in english are situational. Also some languages may have sylables that do not include vowels and such. 
